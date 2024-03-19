89f10f4fae106065bc2daff721cc6211ba43fee5
erisa zaimi <ezaimi21@epoka.edu.al>
2024-03-19 19:08:09 +0100

    starter

---
+++ back-end/src/main/java/com/example/kosherja/KosherjaApplication.java
@@ -0,0 +1,13 @@
+package com.example.kosherja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KosherjaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KosherjaApplication.class, args);
+	}
+
+}
---
+++ back-end/src/test/java/com/example/kosherja/KosherjaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kosherja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KosherjaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
