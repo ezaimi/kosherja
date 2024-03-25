3cef0f855dfdb0523289a9342d2a8e71d565da2a
erisa zaimi <ezaimi21@epoka.edu.al>
2024-03-25 23:53:45 +0100

    performance evaluation models

---
+++ back-end/src/main/java/com/example/kosherja/Model/PerformanceEvaluation/EvaluationForm.java
@@ -0,0 +1,22 @@
+package com.example.kosherja.Model.PerformanceEvaluation;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.List;
+
+@Data
+@Document(collection = "evaluation")
+public class EvaluationForm {
+
+    @Id
+    private String id;
+
+    private String title;
+
+    private String description;
+
+    private List<QuestionType> questions;
+
+}
---
+++ back-end/src/main/java/com/example/kosherja/Model/PerformanceEvaluation/Question.java
@@ -0,0 +1,13 @@
+package com.example.kosherja.Model.PerformanceEvaluation;
+
+import java.util.List;
+
+public class Question {
+
+    private String text;
+
+    private QuestionType type;
+
+    private List<String> options; //  used for multiple choice or dropdown questions
+
+}
