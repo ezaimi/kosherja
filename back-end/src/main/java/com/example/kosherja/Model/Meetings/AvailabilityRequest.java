03589eb737742d42725bcfe5492526d6fc91c929
ArdisaBeqja <abeqja21@epoka.edu.al>
2024-03-28 17:56:38 +0100

    mettingg

--- back-end/src/main/java/com/example/kosherja/Model/Meetings/AvailabilityRequest.java
+++ back-end/src/main/java/com/example/kosherja/Model/Meetings/AvailabilityRequest.java
@@ -2,15 +2,21 @@ package com.example.kosherja.Model.Meetings;
 
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.List;
 
-@Document(collection="AvailableDates")
+@Document(collection="Calendar_AvailableDates")
 @NoArgsConstructor
 @AllArgsConstructor
 public class AvailabilityRequest{
+
+    @Id
+    private  String id;
+    private String managerId;;
     private LocalDate date;
     private boolean available;
 
@@ -29,5 +35,21 @@ public class AvailabilityRequest{
 
     public void setDate(LocalDate date) {
         this.date = date;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getManagerId() {
+        return managerId;
+    }
+
+    public void setManagerId(String managerId) {
+        this.managerId = managerId;
     }
 }
